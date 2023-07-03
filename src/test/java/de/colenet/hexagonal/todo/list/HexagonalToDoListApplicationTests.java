c1f0653eb2a7c6ef74abd22f397986cb6becc1a1
Ricardo Schnur <ricardo.schnur@colenet.com>
2023-07-03 15:09:31 +0200

    Initialize project
    Created a new Spring Boot application. Code compiles and application starts.
    
    Provided a small README on how to build/start the app and on what it is supposed to be able to do in the future.
    
    From a technical point of view:
    - Pulled in some Maven dependencies that I will be using
    - Enabled Java 20 preview features (mainly for pattern matching)
    - Set up a Github Actions CI build
    - Set up Renovate for automatic dependency updates
    - Set up prettier to enforce a consistent code formatting

---
+++ src/test/java/de/colenet/hexagonal/todo/list/HexagonalToDoListApplicationTests.java
@@ -0,0 +1,11 @@
+package de.colenet.hexagonal.todo.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
